3bd0cd7be45c700eb9b13b541c795212c343391d
Ashish Gupta <engashish@gmail.com>
2017-03-05 18:08:00 +0530

    day27

---
+++ src/main/Day27.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.Scanner;
+
+
+public class Day27 {
+
+	
+	public static void main(String args[])
+	{
+
+		System.out.println("4 3");
+		System.out.println("-1 0 1 2");
+
+		System.out.println("3 1");
+		System.out.println("-1 0 1");
+
+		System.out.println("5 4");
+		System.out.println("-1 0 1 2 3");
+
+		System.out.println("6 1");
+		System.out.println("-1 0 1 2 3 4");
+
+		System.out.println("7 6");
+		System.out.println("-1 0 1 2 3 4 5");
+		
+	}
+	
+
+}
+
+
